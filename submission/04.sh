ff56c9afa288aa5a8bf4b53d16e98520bd14bd6b
Zeegaths <your.email@example.com>
2025-03-27 12:53:39 +0300

    submission

--- submission/04.sh
+++ submission/04.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # List all unspent transaction outputs (UTXOs) in the wallet
-bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent || echo "[]
+bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent || echo "[]"
