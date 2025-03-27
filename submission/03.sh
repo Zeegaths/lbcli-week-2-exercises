eb205fb716ef596a9f7fdac66af472ab268b6cdb
Zeegaths <your.email@example.com>
2025-03-27 12:18:49 +0300

    submission

--- submission/03.sh
+++ submission/03.sh
@@ -4,8 +4,7 @@
 SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "segwit_address" "bech32")
 
 # Add some funds to the address with explicit fee rate
-# Using send command with explicit fee rate to avoid fee estimation issues
-bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$SEGWIT_ADDRESS" 1.0 "" "" true 6 "unset" 0.00001
+bitcoin-cli -regtest -rpcwallet=btrustwallet sendtoaddress "$SEGWIT_ADDRESS" 1.0 "" "" true
 
 # Return only the address
 echo "$SEGWIT_ADDRESS"
