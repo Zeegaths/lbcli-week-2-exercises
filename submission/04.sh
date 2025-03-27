33e918c1c368b16100de427e532d46c732d8fb26
Zeegaths <your.email@example.com>
2025-03-27 13:02:36 +0300

    submission

--- submission/04.sh
+++ submission/04.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-# List all unspent transaction outputs (UTXOs) in the wallet
-OUTPUT=$(bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent 2>/dev/null)
+bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent
 
-# Check if the command succeeded
-if [ $? -eq 0 ]; then
-  echo "$OUTPUT"
-else
-  # If there are no UTXOs yet, generate some
-  ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress)
-  bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS" >/dev/null 2>&1
-  
-  # Now list UTXOs again
-  bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent
-fi
